2c7721b90429ade7152b83122a23308e1faec2c1
Trinh Hoang Duy <duyduyduy648@gmail.com>
2026-04-28 11:36:03 +0700

    Lưu tạm code để test

---
+++ src/main/java/com/auction/client/Launcher.java
@@ -0,0 +1,8 @@
+package com.auction.client;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Gọi hàm main của MainApp từ đây để lách luật kiểm tra của Java
+        com.auction.MainApp.main(args);
+    }
+}
