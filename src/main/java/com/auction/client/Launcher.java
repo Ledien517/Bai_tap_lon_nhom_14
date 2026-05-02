623d3391fa18a8aac859c21f41c752c1af5a6547
duydo <25020083@vnu.edu.vn>
2026-05-02 22:47:01 +0700

    thêm từ jde

--- src/main/java/com/auction/client/Launcher.java
+++ src/main/java/com/auction/client/Launcher.java
@@ -4,5 +4,6 @@ public class Launcher {
     public static void main(String[] args) {
         // Gọi hàm main của MainApp từ đây để lách luật kiểm tra của Java
         MainApp.main(args);
+        //duudyudydudyud
     }
 }
